4dfb45e8216c1a3bc303efa588da41951343aa80
DikkeTimo <62908209+DikkeTimo@users.noreply.github.com>
2023-05-10 12:26:21 +0200

    update localstorage

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 let darkToggle = document.getElementById("toggle");
+let isLight = localStorage.getItem("isLight") === "true";
+
+if (isLight) {
+  document.body.classList.add("light");
+}
 
 darkToggle.addEventListener("click", () => {
   document.body.classList.toggle("light");
+  localStorage.setItem("isLight", document.body.classList.contains("light"));
 });
-
-
